4641b859844ce561ff3a2adb92691489a1ec17b1
Rifat <rifat@Rifats-MacBook-Pro.local>
2026-03-11 23:19:26 +1100

    add env file in both site and add firebase environment v

--- trees-care-client/Firebase/Firebase.init.js
+++ trees-care-client/Firebase/Firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCJccKweWm5VAfeauXYtGUSR8mEL7EBw8",
-  authDomain: "trees-care-app-16fba.firebaseapp.com",
-  projectId: "trees-care-app-16fba",
-  storageBucket: "trees-care-app-16fba.firebasestorage.app",
-  messagingSenderId: "129093381375",
-  appId: "1:129093381375:web:65065b0fac71381ae3f0ca",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
